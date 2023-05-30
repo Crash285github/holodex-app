77e55da08af9b3a40b3402861cbf2cf6f248aaf6
Crash285 <91914424+Crash285github@users.noreply.github.com>
2023-05-30 22:36:49 +0200

    adding deploy script (#1)
    * test
    
    * test

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LEarned Rae t
         </a>
       </header>
     </div>
