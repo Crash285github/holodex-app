2f1a397e1c6b7cfd1623d4eacd2c057f64ec7448
Horváth Ádám <jackadam852@gmail.com>
2023-07-06 17:57:23 +0200

    infinitescroll

--- src/Components/Body/Body.tsx
+++ src/Components/Body/Body.tsx
@@ -2,14 +2,16 @@ import { useEffect, useState } from 'react';
 import './Body.css'
 import useFetch from '../../useFetch';
 import TalentCard, { Talent } from '../TalentCard/TalentCard';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const Body = () => {
+
   const [talents, setTalents] = useState<Talent[]>()
   const [offset, setOffset] = useState(10)
   const [canLoad, setCanLoad] = useState(true)
   const [newTalentsPending, setNewTalentsPending] = useState(false)
 
-  const {data, isPending, error} = useFetch(
+  const {data} = useFetch(
     {
       url: 'https://holodex.net/api/v2/channels?type=vtuber&limit=10&org=Hololive&offset=0',
       options: {
@@ -22,11 +24,12 @@ const Body = () => {
       setTalents(data)  
     },[data])
 
+  const steps = 10
 
   const handleClick = () => {
     setNewTalentsPending(true)
     const params = {
-      url: 'https://holodex.net/api/v2/channels?type=vtuber&limit=10&org=Hololive&offset=' + offset,
+      url: 'https://holodex.net/api/v2/channels?type=vtuber&limit='+steps+'&org=Hololive&offset=' + offset,
       options: {
       method: 'GET',
       headers: {Accept: 'application/json', 'X-APIKEY': 'e25fc430-10c6-490a-b7e8-af76fc275cfd'}
@@ -45,10 +48,10 @@ const Body = () => {
         const newTalents = talents
         newTalents?.push(...data)
         setTalents(newTalents)
-        setOffset(offset + 10)
+        setOffset(offset + steps)
         setNewTalentsPending(false)
 
-        if(data.length < 10){
+        if(data.length < steps){
           setCanLoad(false)
         }
       })
@@ -63,29 +66,19 @@ const Body = () => {
   return ( 
     <>
       <div className="body">
-        { isPending && 
-        <div className='loading'>Loading...</div>
-        }
-
-        { error && 
-          <div className='error'>Error: {error}</div>
-        }
-
-        { talents &&
-        <>
+        <InfiniteScroll 
+          dataLength={talents ? talents.length : 0}
+          next={handleClick}
+          hasMore={canLoad}
+          loader={<div className='loading'>Loading...</div>}
+        >
           <div className="talent-list">
             {talents?.map((talent) => (
               <TalentCard {...talent} key={talent.id}></TalentCard>
               ))}
           </div>
-          { canLoad &&
-            <button className='more-talents' disabled={newTalentsPending}
-              onClick={handleClick}>More talents</button>
-          }
-        </>
-        }
-      </div>
-      
+        </InfiniteScroll>
+      </div>     
     </>
    );
 }
